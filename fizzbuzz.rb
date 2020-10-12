36ad94e1459720ca490dfc29e654d69cc69da4e5
Jacob Tobias <jtobias811@gmail.com>
2020-10-12 18:11:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if (int % 3 && int % 5) == 0
-    returns "FizzBuzz"
+    puts "FizzBuzz"
   elsif int % 3 == 0
-    returns "Fizz"
+    puts "Fizz"
   elsif int % 5 == 0
-    returns "Buzz"
+    puts "Buzz"
   else
     returns "nil"
   end
