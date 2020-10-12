8adad2ada2e68bf29c1e7cdcf8349309f5e386ce
Jacob Tobias <jtobias811@gmail.com>
2020-10-12 18:24:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,13 @@
 def fizzbuzz(int)
   if (int % 3 && int % 5) == 0
     return "FizzBuzz"
+  end
   if int % 3 == 0
     return "Fizz"
+  end
   elsif int % 5 == 0
     return "Buzz"
+  end
   else
     return "nil"
   end
